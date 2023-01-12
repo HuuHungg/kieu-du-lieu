39f98f0435767eed7ae1365ab6b5c5cc3fccebb7
Nguyễn Hữu Hưng <nguyenhuuhung@Nguyens-MacBook-Air.local>
2023-01-12 10:52:19 +0700

    add file logical

--- index.js
+++ index.js
@@ -1,50 +1,22 @@
-let a = 1 , b = 2 , c = 3;
-console.log(a+b+c)
 
-let fullName = 'Huu Hung';
-let lasName = 'ReacJS at fullstack';
-console.log(`${fullName} dang hoc ${lasName} `)
-console.log(typeof fullName)
+let result = 'A' && 'B' && 0 && 'D';
 
-let firstName = `勉強してるがんばりましょう　'でも難しいですががんぱってくだい '`
-console.log(firstName)
-
-let isSuccess = true;
-console.log(isSuccess)
-
-let age 
-console.log(age)
-
-let isNull = null
-console.log(isNull)
-
-let id = Symbol('id') // unique
-let id2 = Symbol('id') // unique
-
-console.log(id === id2)
-
-// Function
-// let myFunction = function() {
-//     alert('毎日2時間ぐらい日本語を勉強することにしてる')
-// }
-
-// myFunction()
-
-// Object
-
-let myObject = {
-    name: 'Huu Hung',
-    age: 20,
-    address: 'Ha Noi' ,
-    myFunction: function() {
-        console.log(勉強してる)
-    }
+if(result) {
+    console.log('Dieu Kien Dung')
+}else {
+    console.log('Dieu Kien Sai')
 }
 
-console.log('Myobject', myObject)
 
-let myArray = [
-    'Javascript', 'PHP', 'Ruby',
-]
+let a = 1
+let b = 2
 
-console.log(typeof myArray)
+
+let result2 = null || 0 || false;
+
+if(result2) {
+    console.log('勉強してる')
+}else {
+    console.log('勉強じゃない')
+}
+
