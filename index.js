ea7791717e89cc02ab9cdc5a7dc80baf8618cc2c
Nguyễn Hữu Hưng <nguyenhuuhung@Nguyens-MacBook-Air.local>
2023-01-12 23:09:01 +0700

    add file function

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
 
-let result = 'A' && 'B' && 0 && 'D';
+function writeLog(message) {
+    let myString = ''
+    for(var param of arguments) {
+        myString += `Nguyen Huu Hung dang hoc ${param} -`
+    }
 
-if(result) {
-    console.log('Dieu Kien Dung')
-}else {
-    console.log('Dieu Kien Sai')
+    console.log(myString)
 }
 
-
-let a = 1
-let b = 2
-
-
-let result2 = null || 0 || false;
-
-if(result2) {
-    console.log('勉強してる')
-}else {
-    console.log('勉強じゃない')
-}
-
+writeLog(['javascript', 'PHP', 'Ruby','Mysql'], ['fe', 'be'], ['react'])
