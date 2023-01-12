0c69d0e6b38784942fcadd75b5ff43e390a8f5ef
Nguyễn Hữu Hưng <nguyenhuuhung@Nguyens-MacBook-Air.local>
2023-01-12 23:23:12 +0700

    add file modifle

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 
+
+
+
+
+
 function writeLog(message) {
     let myString = ''
     for(var param of arguments) {
@@ -9,3 +14,11 @@ function writeLog(message) {
 }
 
 writeLog(['javascript', 'PHP', 'Ruby','Mysql'], ['fe', 'be'], ['react'])
+
+
+function sum(a,b) {
+    return a + b
+}
+
+let result = sum(20,30)
+console.log(result)
