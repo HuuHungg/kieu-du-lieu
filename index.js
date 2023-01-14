73c537372e9d012a12731ca319d6132b5dc5c538
Nguyễn Hữu Hưng <nguyenhuuhung@Nguyens-MacBook-Air.local>
2023-01-14 20:40:13 +0700

    add file modifle

--- index.js
+++ index.js
@@ -1,21 +1,45 @@
-// Cach 1
-let myArray = [
+let course = [
     'Javascript',
     'PHP',
     'Ruby',
-    undefined,
-    null,
+    'PHP'
 ]
 
-console.log(Array.isArray(myArray))
+// to string
+console.log(course.toString())
 
+// Join hoạt động như string, có thể chỉ đinhj dấu phân cách
+console.log(course.join(' - '))
 
-// Cach 2
+// Pop Xoá element ở cuối mảng và trả về phần tử đã xoá
+console.log(course.pop()) // PHP
+console.log(course)
 
-let myArraydemo = new Array ('PHP', 'ReactJS', 'NodeJS')
-console.log(myArraydemo)
+// Push thêm một hoặc nhiều phần tử ở cuối mảg và trả về độ dài mới của mảng
 
+let fruit = ['Orang', 'Apple']
+fruit.push('Mango', 'Banana',)
+console.log(fruit)
 
-let fullCourse = ['mySql', 'file', 'react']
-console.log(fullCourse[0])
+// Shift xoá phần tử ở đầu mảng và trả về phần tử đã xoá
+console.log(course.shift()) // Javascript
+console.log(course)
 
+// Unshif thêm một hoặc nhiều phần tử ở đầu mảng và trả về độ dài mới của mảng
+let fruit2 = ['Orang', 'Apple',]
+fruit2.unshift('watermelon')
+console.log(fruit2)
+
+// Splice : cắt xoá chèn phần tử vào mảng, (vị trí bắt đầu, số phần tử muốn xoá, element muốn thêm vào)
+let game = ['nr', 'lw','lm']
+game.splice(1,0,'dra','mobile','online')
+console.log(game)
+
+// Concat: Nối mảng array với nhau
+
+let mergeCourse = fruit2.concat(game)
+console.log(mergeCourse)
+
+// Slicing (slice): cắt một vài phần tử của mảng (Ví trí đầu tiên là bắt đầu cắt, vị trí thứ hai là kết thúc cắt)
+let fullCourse = ['Rect', 'MySQL', 'FE', 'BE']
+console.log(fullCourse.slice(3))
