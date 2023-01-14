8eb1b40232c3fea41ad9225ee3fdda419b74ba8a
Nguyễn Hữu Hưng <nguyenhuuhung@Nguyens-MacBook-Air.local>
2023-01-14 09:33:22 +0700

    add file string

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
+var fullName = 'Huu Hung Nguyen'
+console.log(fullName)
+
+let firstName = new String('勉強してるがんばろ')
+console.log(firstName)
+
+let lastName = `毎日"２時間ぐらいITを勉強してる" \\ で難しいです `
+console.log(lastName.length)
+
+let name = "毎日日本語を勉強してるでも難しいですががんばろ "
++ "毎日日本語を勉強してるでも難しいですががんばろ"
++ "毎日日本語を勉強してるでも難しいですががんばろ"
++ "毎日日本語を勉強してるでも難しいですががんばろ"
+
+console.log(name)
 
 
+// Template String
+let maeName = 'Huu Hung'
+let ushiroName = 'dang hoc it'
 
-
-
-
-function writeLog(message) {
-    let myString = ''
-    for(var param of arguments) {
-        myString += `Nguyen Huu Hung dang hoc ${param} -`
-    }
-
-    console.log(myString)
-}
-
-writeLog(['javascript', 'PHP', 'Ruby','Mysql'], ['fe', 'be'], ['react'])
-
-
-function sum(a,b) {
-    return a + b
-}
-
-let result = sum(20,30)
-console.log(result)
+console.log(`Toi la ${maeName} ${ushiroName} nhung kho qua ganbarimasho`)
