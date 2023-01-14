9360e0523071a73b1cc35bc65372b9ef35355ad0
Nguyễn Hữu Hưng <nguyenhuuhung@Nguyens-MacBook-Air.local>
2023-01-14 11:11:10 +0700

    add file modfile

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
-var fullName = 'Huu Hung Nguyen'
-console.log(fullName)
 
-let firstName = new String('勉強してるがんばろ')
-console.log(firstName)
+let myString = "毎日 JS 日本語を勉強してるがんばりましょう JS";
+let myString2 = "Hoc JS tai F8 JS"
+let myString3 = "   勉強してる　　"
+// length
+console.log(myString.length)
 
-let lastName = `毎日"２時間ぐらいITを勉強してる" \\ で難しいです `
-console.log(lastName.length)
+// Find index
+// Tìm vị trí JS cuối cùng
+console.log(myString.lastIndexOf('JS'))
 
-let name = "毎日日本語を勉強してるでも難しいですががんばろ "
-+ "毎日日本語を勉強してるでも難しいですががんばろ"
-+ "毎日日本語を勉強してるでも難しいですががんばろ"
-+ "毎日日本語を勉強してるでも難しいですががんばろ"
+// Tìm vị trí js đầu tiên 
+console.log(myString.indexOf('JS',7))
 
-console.log(name)
+// Tìm kiếm với search
+console.log(myString.search('JS'))
 
+// 3. Cut string
+console.log(myString2.slice(4))
+console.log(myString2.slice(-3, -1))
+console.log(myString2.substring(4,6))
 
-// Template String
-let maeName = 'Huu Hung'
-let ushiroName = 'dang hoc it'
+// Replace ghi de
+console.log(myString2.replace(/JS/g, 'Javascript'))
 
-console.log(`Toi la ${maeName} ${ushiroName} nhung kho qua ganbarimasho`)
+// Uppercase // Lowercase
+
+console.log(myString2.toUpperCase())
+console.log(myString2.toLowerCase())
+
+// Trim Loại bỏ kí tự thừa
+console.log(myString3.trim())
+
+// split: 
+let Languages = 'Javascript'
+console.log(Languages.split(''))
+
+let fine = 'I am cooking dinner now'
+console.log(fine[40])
