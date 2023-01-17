5fd65d7079552a4cec5e44a29eb67be094752f38
Nguyễn Hữu Hưng <nguyenhuuhung@Nguyens-MacBook-Air.local>
2023-01-17 11:31:12 +0700

    add file modifile

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-let date = 5
 
-if (date === 2) {
-    console.log('To day is Monday')
-} else if (date === 3) {
-    console.log('To day is Tuesday')
-} else if(date === 4) {
-    console.log('To day is Wednesday')
-} else {
-    console.log('I do not know')
+let date = 6
+
+switch (date) {
+    case 2:
+        console.log('Today is Monday')
+        break;
+    case 3:
+        console.log('Today is Tuesday')
+        break;
+    case 4:
+        console.log('Today is Wednesday')
+        break;
+    default:
+        console.log(' I do not known')
 }
 
+// khi mà bỏ break, mà điều kiện đúng thì nó sẽ trả ra tiếp điều kiện tiếp theo đến khi gặp break thì thôi
+// Trước case sẽ là điều kiện
+// khi muốn kiểm tra 3 case trở lên thì dùng switch, ngược lại thì dùng if elses
