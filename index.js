4e2004f38ae0dc7f6c6214b912a2d7e39cf6d8b2
Nguyễn Hữu Hưng <nguyenhuuhung@Nguyens-MacBook-Air.local>
2023-01-17 20:48:40 +0700

    add file modile

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
-let myArray = [
+// Lấy ra từng phẩn tử của một mảng 
+
+let languages = [
     'Javascript',
     'PHP',
-    'Java',
-    'Dart'
+    'Ruby'
 ]
 
-let arrayLength = myArray.length;
+for(var value of languages) {
+    console.log(value)
+}
 
-for(let i = 0; i < arrayLength; i++) {
-    console.log(myArray[i])
+
+let languages2= 'Javascript';
+for(var index of languages2) {
+    console.log(index)
+}
+
+
+// Object 
+
+let myInfo = {
+    name: 'Huu Hung',
+    age: 20
+}
+
+for(var value of Object.keys(myInfo)) {
+    console.log(value)
 }
