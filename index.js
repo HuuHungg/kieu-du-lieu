d80c7501feac2bedc57364d498145200da40eb92
Nguyễn Hữu Hưng <nguyenhuuhung@Nguyens-MacBook-Air.local>
2023-01-17 09:30:05 +0700

    add file modifile

--- index.js
+++ index.js
@@ -1,23 +1,9 @@
-function Person(firstName,lastName,age,eyeColor) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.age = age;
-    this.eyeColor = eyeColor;
+let date = new Date()
 
-    this.getName = function() {
-        return `${this.firstName} ${this.lastName} ${this.age} ${this.eyeColor} `
-    }
-}
+let month = date.getMonth()
+let year = date.getFullYear()
+let day = date.getDay()
+let Hours = date.getHours()
+let Second = date.getSeconds()
 
-
-Person.prototype.className = 'F8';
-Person.prototype.getClassName = function() {
-    return this.className;
-}
-
-
-let myMother = new Person('Sally','Rally',40,"black")
-let myFather = new Person('Fine', 'Cake', 45, "blue")
-
-console.log(myMother.className)
-console.log(myFather.getClassName())
+console.log(`${day}/${month}/${year}/${Hours}/${Second}`)
