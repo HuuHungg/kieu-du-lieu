ae6080d2b0e7c8381bde00c1cc525919511d0944
Huu Hung <huuhungnguyen2002@gmail.com>
2023-01-27 16:21:06 +0700

    add file modifile

--- index.js
+++ index.js
@@ -1,46 +1,47 @@
-Array.prototype.some2 = function(cb) {
-    let output = false
-
-    for( let index in this) {
+Array.prototype.every2 = function(cb) {
+    
+    let output = true
+    
+    for(let index in this) {
         if(this.hasOwnProperty(index)) {
-            if(cb(this[index], index, this)) {
-                output = true;
-                break
+            let result = cb(this[index], index, this)
+            if(!result) {
+                output = false;
+                break;
             }
-
         }
     }
+    
     return output
 }
-
 
 
 let courses = [
     {
         name:'Javascript',
         coin: 680,
-        isFinish: false,
+        isFinish: true,
     },
     {
         name: 'PHP',
         coin: 690,
-        isFinish: false,
+        isFinish: true,
     },
     {
         name: 'NodeJS',
         coin: 710,
-        isFinish: true
+        isFinish: true,
     },
     {
         name: 'ReactJS',
         coin: 800,
-        isFinish: false,
+        isFinish: true,
 
     }
 ]
 
 // Tối thiểu một phần tử thoả mãn là trả về true
-let result = courses.some2(function(course,index) {
+let result = courses.every2(function(course,index,arr) {
     return course.isFinish
 })
 
