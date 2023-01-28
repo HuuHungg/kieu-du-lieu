da05276b4bcfea5e2335435b17542636940a7205
Huu Hung <huuhungnguyen2002@gmail.com>
2023-01-28 10:53:32 +0700

    add file modifile

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+let headings = document.querySelectorAll('.heading')
+console.log(headings)
 
-let result = document.querySelector('.box-1')
-console.log(result)
-
-console.log(result.getElementsByTagName('li'))
-console.log(result.querySelector('p'))
+for(let i = 0; i < headings.length ; i++) {
+    console.log(headings[i])
+}
+console.log(document.forms['form-2'])
+document.write('Hello guys')
