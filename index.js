b8baa0ef1f1ab53112f0147e0b6e3c3477e8eed3
Huu Hung <huuhungnguyen2002@gmail.com>
2023-01-28 10:17:41 +0700

    add file modfile

--- index.js
+++ index.js
@@ -1,36 +1,6 @@
-// HTML DOM
-// 1. Element: ID, class, tag, CSS  selector, HTML collection
-// 2. Attribute
-// 3. Text
 
-
-// ID
-let result = document.getElementById('heading')
+let result = document.querySelector('.box-1')
 console.log(result)
 
-// ClassName
-
-let headingNodes = document.getElementsByClassName('heading2')
-console.log(headingNodes)
-
-// Tag Name: Lấy ra tên của thẻ
-let tagNodes = document.getElementsByTagName('h2')
-console.log(tagNodes)
-let pNodes = document.getElementsByTagName('p')
-console.log(pNodes)
-
-// CSS selector
-
-let selector = document.querySelector('.box .heading-3:nth-child(3)') 
-// Muốn lấy ra tất cả thì dùng querySelectorAll     
-console.log(selector)
-
-let selector2 = document.querySelectorAll('.heading-3')
-console.log(selector2)
-
-// HTML Collection
-// anchors: Lấy ra tất cả thẻ a có atribute là Name
-
-console.log(document.anchors);
-
-
+console.log(result.getElementsByTagName('li'))
+console.log(result.querySelector('p'))
