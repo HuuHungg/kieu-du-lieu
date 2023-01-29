dad37b7c856892f0a22db41eb0b51f378838cb78
Huu Hung <huuhungnguyen2002@gmail.com>
2023-01-29 21:42:16 +0700

    add file modfile

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
+// ClassList prototype
+
+
+// add: Thêm class vào Element 
+// contains: Kiểm tra xem 1 class có nằm trong element này hay không
+// remove: Xoá bỏ class khỏi element của các bạn 
+// Toggle xoá bỏ class đó, có thì nó thêm không có thì nó sẽ gỡ bỏ
+
+
+
+// add 
 let boxElement = document.querySelector('.box');
+    boxElement.classList.add('red', 'blue')
 
-// Cách 1
-boxElement.style.width = '100px',
-boxElement.style.height = '200px'
-boxElement.style.backgroundColor = 'red'
+// contains:  Kiểm tra class xem có tồn tại hay không
 
-// Cách 2
+console.log(boxElement.classList.contains('red'))
 
-Object.assign(boxElement.style, {
-    with: '200px',
-    height: '100px',
-    backgroundColor: 'green',
-    
-})
+// Để xoá thì dùng remove 
 
-// Khi muốn lấy ra một thuộc tính trong element style
-console.log(boxElement.style.backgroundColor) 
-console.log(boxElement.style.width)
+boxElement.classList.remove('blue')
+
+setTimeout(() => {
+    boxElement.classList.remove('red')
+},3000)
+
+
+// Tonggle: 
+
+setInterval(() => {
+    boxElement.classList.toggle('red')
+},2000)
+
