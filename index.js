e2b8b0542024f3903f202d748186ca6d57a8d69f
Huu Hung <huuhungnguyen2002@gmail.com>
2023-01-30 16:42:23 +0700

    add file modfile

--- index.js
+++ index.js
@@ -1,56 +1,51 @@
-// DOM events
+// 1. preventDefault: Bỏ đi hành vi mặc định 
 
-let inputValue;
+// let aElement = document.links;
 
-let inputElement = document.querySelector('input[type="text"]')
-console.log(inputElement)
+// for(let i = 0; i < aElement.length; ++i) {
+//     aElement[i].onclick = function(e) {
 
-inputElement.onchange = function(e) {
- // .value Lấy giá trị của thẻ input, .target: lấy ra được chính giá trị của elêmnt node     
-    inputValue = e.target.value
-}
+//         if(!e.target.href.startsWith('https://f8.edu.vn')) {
+//             e.preventDefault();
+//         }
+//     }
+// }
 
+let aElement = document.links
+let aLength = aElement.length
 
-let checkElement = document.querySelector('input[type="checkbox"]')
-
-checkElement.onchange = function(e) {
-    console.log(e.target.value)
-}
-
-let selectElement = document.querySelector('select')
-    selectElement.onchange= (e) => {
-        console.log(e.target.value)
-    }
-
-
-Keyup / keydown
-
-let inputElement = document.querySelector('input[type="text"]')
-    inputElement.onkeyup= (e) => {
-        console.log(e.which)
-    
-    switch(e.which) {
-        case 27:
-            console.log('Exit')
-            break;
-        
-        case 65: 
-            console.log('Huu Hun Nguyen')
-            break;
-        
+for(let i=0; i< aLength; ++i) {
+    aElement[i].onclick = function(e) {
+        if(!e.target.href.startsWith('https://google.com.vn')) {
+            e.preventDefault()
+        }
     }
 }
 
 
-
-
-document.onkeydown = function(e) {
-    switch(e.which) {
-        case 13:
-            console.log('Huu Hung Nguyen dang hoc lap trinh')
-            break;
-        case 66: 
-            console.log('IT that la kho')
-            break;
-    }
+let ulElement = document.querySelector('ul');
+ulElement.onmousedown = function(e) {
+    e.preventDefault()
 }
+
+
+ulElement.onclick = function(e) {
+    console.log(e.target)
+}
+
+
+// 2. stopPropagation: Ngăn chặn hành vi nổi bọt lan truyền
+
+
+document.querySelector('div').onclick = 
+    function() {
+        console.log('DIV')
+    }
+
+document.querySelector('button').onclick = 
+    function(e) {
+        e.stopPropagation();
+        console.log('Click me')
+    }
+
+
