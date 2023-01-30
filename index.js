5be9ec711b03245f58bd0eabb7de52783a66bad3
Huu Hung <huuhungnguyen2002@gmail.com>
2023-01-30 14:15:07 +0700

    add file modfile

--- index.js
+++ index.js
@@ -1,33 +1,14 @@
-// ClassList prototype
+// DOM events
+
+// 1. Attribute events
+// 2 . Assign event using the element node
+
+let h1Elements = 
+    document.querySelectorAll('h1')
+
+for (let i = 0 ; i < h1Elements.length; i++) {
+    h1Elements[i].onclick = (e) => console.log(e.target)
+}
 
 
-// add: Thêm class vào Element 
-// contains: Kiểm tra xem 1 class có nằm trong element này hay không
-// remove: Xoá bỏ class khỏi element của các bạn 
-// Toggle xoá bỏ class đó, có thì nó thêm không có thì nó sẽ gỡ bỏ
-
-
-
-// add 
-let boxElement = document.querySelector('.box');
-    boxElement.classList.add('red', 'blue')
-
-// contains:  Kiểm tra class xem có tồn tại hay không
-
-console.log(boxElement.classList.contains('red'))
-
-// Để xoá thì dùng remove 
-
-boxElement.classList.remove('blue')
-
-setTimeout(() => {
-    boxElement.classList.remove('red')
-},3000)
-
-
-// Tonggle: 
-
-setInterval(() => {
-    boxElement.classList.toggle('red')
-},2000)
 
