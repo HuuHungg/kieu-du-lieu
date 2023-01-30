a82149b7daf7601dd08eb624d3d846c9cfda8e15
Huu Hung <huuhungnguyen2002@gmail.com>
2023-01-30 15:19:18 +0700

    add file modfile

--- index.js
+++ index.js
@@ -1,14 +1,56 @@
 // DOM events
 
-// 1. Attribute events
-// 2 . Assign event using the element node
+let inputValue;
 
-let h1Elements = 
-    document.querySelectorAll('h1')
+let inputElement = document.querySelector('input[type="text"]')
+console.log(inputElement)
 
-for (let i = 0 ; i < h1Elements.length; i++) {
-    h1Elements[i].onclick = (e) => console.log(e.target)
+inputElement.onchange = function(e) {
+ // .value Lấy giá trị của thẻ input, .target: lấy ra được chính giá trị của elêmnt node     
+    inputValue = e.target.value
+}
+
+
+let checkElement = document.querySelector('input[type="checkbox"]')
+
+checkElement.onchange = function(e) {
+    console.log(e.target.value)
+}
+
+let selectElement = document.querySelector('select')
+    selectElement.onchange= (e) => {
+        console.log(e.target.value)
+    }
+
+
+Keyup / keydown
+
+let inputElement = document.querySelector('input[type="text"]')
+    inputElement.onkeyup= (e) => {
+        console.log(e.which)
+    
+    switch(e.which) {
+        case 27:
+            console.log('Exit')
+            break;
+        
+        case 65: 
+            console.log('Huu Hun Nguyen')
+            break;
+        
+    }
 }
 
 
 
+
+document.onkeydown = function(e) {
+    switch(e.which) {
+        case 13:
+            console.log('Huu Hung Nguyen dang hoc lap trinh')
+            break;
+        case 66: 
+            console.log('IT that la kho')
+            break;
+    }
+}
