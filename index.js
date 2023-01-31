7b415cc309336b990280ba9d3c6378d4127ea00f
Huu Hung <huuhungnguyen2002@gmail.com>
2023-01-31 18:01:26 +0700

    add file modfile

--- index.js
+++ index.js
@@ -1,26 +1,101 @@
-function sleep(ms) {
-    return new Promise((resolve) => {
-        setTimeout(resolve, ms)
+ let users = [
+    {
+        id: 1,
+        name: 'Huu Hung'
+    },
+    {
+        id: 2,
+        name: 'Son Dang'
+    }   ,
+    {
+        id: 3,
+        name: 'Kien Dam'
+    }
+ ]
+
+ let comments = [
+    {
+        id: 1,
+        user_id: 1,
+        content: 'Anh Son chua ra video :('
+        
+    },
+    {
+        id: 2,
+        user_id: 2,
+        content: 'Vua ra xong em oi !'
+    },
+    {
+        id: 3,
+        user_id: 1,
+        content: 'Cam on anh ^^'
+    },
+    {
+        id: 4,
+        user_id:2,
+        content: 'Khong co gi em co gang hoc nhe'
+    }
+
+ ]
+
+ // 1. Lấy comments
+ // 2. Từ comments lấy ra user_id
+ // 3, Từ user_id lấy ra user tương ứng
+
+ // Fake API
+ function getComments () {
+    return new Promise (function(resolve) {
+        setTimeout(function() {
+            resolve(comments)
+        }, 1000)
+    })
+ }
+
+
+function getUsersByIds(userIds) {
+    return new Promise(function(resolve) {
+        let result = users.filter(function(user) {
+            return userIds.includes(user.id)
+        })
+        setTimeout (function() {
+            resolve(result)
+        }, 1000)
+        
     })
 }
 
-sleep(1000)
-    .then(() => {
-        console.log('Huu Hung')
-        return sleep(1000)
+
+
+ getComments()
+    .then(function(comments) {
+        let userIds = comments.map(function(comment) {
+            return comment.user_id
+        })
+        return getUsersByIds (userIds)
+        .then(function(users) {
+            return {
+                users: users,
+                comments: comments
+            }
+        })
     })
 
-    .then(() => {
-        console.log('Huu Hung Nguyen dang hoc')
-        return sleep(1000)
+    .then(function(data) {
+
+        let commentBlock = document.getElementById('comment-block')
+      
+        let html = ''
+        data.comments.forEach(function(comment) {
+            let user = data.users.find(function(user) {
+                return user.id === comment.user_id
+            })
+            html += `<li>${user.name}: ${comment.content}</li>`
+        })
+        
+        commentBlock.innerHTML = html;
     })
 
-    .then(()=> {
-        console.log('May toan that bai thoi')
-        return sleep(1000)
-    })
 
-    .then(() => {
-        console.log('May phai co len')
-        return sleep(1000)
-    })
+
+
+
