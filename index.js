232b251c7ad5a70fad631aa808d33302b55dd221
Huu Hung <huuhungnguyen2002@gmail.com>
2023-02-01 15:19:48 +0700

    add file modifle

--- index.js
+++ index.js
@@ -1,101 +1,93 @@
- let users = [
+let users = [
     {
-        id: 1,
-        name: 'Huu Hung'
-    },
+        id:1,
+        name: 'Kien Dam',
+    } ,
     {
-        id: 2,
-        name: 'Son Dang'
-    }   ,
-    {
-        id: 3,
-        name: 'Kien Dam'
+        id:2,
+        name: 'Son Dang',
     }
- ]
+];
 
- let comments = [
+let comments = [
     {
         id: 1,
         user_id: 1,
-        content: 'Anh Son chua ra video :('
-        
+        content: 'anh son chua ra video' 
     },
     {
         id: 2,
         user_id: 2,
-        content: 'Vua ra xong em oi !'
+        content: 'Vua ra xong em oi'
     },
     {
         id: 3,
-        user_id: 1,
-        content: 'Cam on anh ^^'
-    },
-    {
-        id: 4,
-        user_id:2,
-        content: 'Khong co gi em co gang hoc nhe'
+        user_id:1,
+        content: 'Cam on anh'
     }
+    
+]
 
- ]
+// 1. Lay comments
+// 2. Tu comments lay ra user_id
+// 3. Tu user_id lay ra user tuong ung
 
- // 1. Lấy comments
- // 2. Từ comments lấy ra user_id
- // 3, Từ user_id lấy ra user tương ứng
+// Fake API
 
- // Fake API
- function getComments () {
-    return new Promise (function(resolve) {
+function getComments () {
+    return new Promise((resolve) => {
         setTimeout(function() {
-            resolve(comments)
-        }, 1000)
-    })
- }
+                resolve(comments)
+        },1000)
+    });
+}
 
 
-function getUsersByIds(userIds) {
-    return new Promise(function(resolve) {
-        let result = users.filter(function(user) {
+function getUsersByIds (userIds) {
+    return new Promise ((resolve)=> {
+        let result = users.filter((user) => {
             return userIds.includes(user.id)
         })
-        setTimeout (function() {
+        setTimeout(() => {
             resolve(result)
-        }, 1000)
-        
+        })
     })
 }
 
 
 
- getComments()
-    .then(function(comments) {
-        let userIds = comments.map(function(comment) {
-            return comment.user_id
-        })
-        return getUsersByIds (userIds)
-        .then(function(users) {
-            return {
-                users: users,
-                comments: comments
-            }
-        })
-    })
-
-    .then(function(data) {
-
-        let commentBlock = document.getElementById('comment-block')
-      
-        let html = ''
-        data.comments.forEach(function(comment) {
-            let user = data.users.find(function(user) {
-                return user.id === comment.user_id
-            })
-            html += `<li>${user.name}: ${comment.content}</li>`
+getComments()
+    .then((comments)=>{
+        let userIds = comments.map((course) => {
+            return course.user_id
         })
         
-        commentBlock.innerHTML = html;
+    return getUsersByIds(userIds)
+        .then(function(users) {
+            return {
+                users: users,   
+                comments: comments,
+            }
+        })    
     })
 
+    .then((data) => {
+        let commentBlock = document.getElementById('comment-block')
+        
+        let html = ''
+        data.comments.forEach(function(comment) {
+            let user = data.users.find((user) => {
+                return user.id === comment.user_id
+            })
+
+            html += `<li> ${user.name}: ${comment.content} </li> `
+        })
+        commentBlock.innerHTML = html
+    }) 
+
+    
 
 
 
-
+    
+    
