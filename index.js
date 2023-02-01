ece3090372a3c2aeeecdd6b260ff7ca2acbc806c
Huu Hung <huuhungnguyen2002@gmail.com>
2023-02-01 16:38:09 +0700

    add file modifile

--- index.js
+++ index.js
@@ -1,93 +1,27 @@
-let users = [
-    {
-        id:1,
-        name: 'Kien Dam',
-    } ,
-    {
-        id:2,
-        name: 'Son Dang',
-    }
-];
+// Fetch
 
-let comments = [
-    {
-        id: 1,
-        user_id: 1,
-        content: 'anh son chua ra video' 
-    },
-    {
-        id: 2,
-        user_id: 2,
-        content: 'Vua ra xong em oi'
-    },
-    {
-        id: 3,
-        user_id:1,
-        content: 'Cam on anh'
-    }
+
+let postApi = 'https://jsonplaceholder.typicode.com/posts';
+
+// stream
+fetch(postApi)
+    .then((response) => {
+        return response.json()
     
-]
-
-// 1. Lay comments
-// 2. Tu comments lay ra user_id
-// 3. Tu user_id lay ra user tuong ung
-
-// Fake API
-
-function getComments () {
-    return new Promise((resolve) => {
-        setTimeout(function() {
-                resolve(comments)
-        },1000)
-    });
-}
-
-
-function getUsersByIds (userIds) {
-    return new Promise ((resolve)=> {
-        let result = users.filter((user) => {
-            return userIds.includes(user.id)
-        })
-        setTimeout(() => {
-            resolve(result)
-        })
-    })
-}
-
-
-
-getComments()
-    .then((comments)=>{
-        let userIds = comments.map((course) => {
-            return course.user_id
-        })
-        
-    return getUsersByIds(userIds)
-        .then(function(users) {
-            return {
-                users: users,   
-                comments: comments,
-            }
-        })    
     })
 
-    .then((data) => {
-        let commentBlock = document.getElementById('comment-block')
-        
-        let html = ''
-        data.comments.forEach(function(comment) {
-            let user = data.users.find((user) => {
-                return user.id === comment.user_id
-            })
-
-            html += `<li> ${user.name}: ${comment.content} </li> `
+    .then((posts) => { 
+        let htmls = posts.map((post) => {
+            return `
+                <h2>${post.title}</h2>
+                <h3> ${post.id} </h3>
+                <p> ${post.body} </p>
+            `
         })
-        commentBlock.innerHTML = html
-    }) 
-
+        let html = htmls.join('')
+        document.getElementById('post-block').innerHTML = html
+    })
     
-
-
-
-    
-    
+    .catch(function(err) {
+        alert('Co loi')
+    })
