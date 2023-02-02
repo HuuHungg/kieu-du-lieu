76d20d137077f2e52d1caa48a6eb4f042548b0bb
Huu Hung <huuhungnguyen2002@gmail.com>
2023-02-02 09:41:39 +0700

    add file modfile

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
-const courseName = 'Javascript';
-const courseName2 = 'day du nhat tai Viet Nam'
-const description = `CourseName: ${courseName} tai f8 ${courseName2} noi suy voi \${}`
 
-console.log(description) 
+// Khi mà viết Arrow function không có dấy {} thì nó hiểu là retrun luôn
+// Còn khi có dấu {} thì nó sẽ hiểu viết khối code trong đó và phải return
+
+const sum = (a,b) => {
+    return {
+        a:a,
+        b:b
+    }
+}
+
+console.log(sum(2,2))
 
 
-let lines = `Line 1
-    line 2
-    line 3`
+const logger = (log) => console.log(log)
+logger('Huu Hung Nguyen ...')
 
-console.log(lines)
+const course = {
+    name: 'Javascript basic',
+    getName: function() {
+       return this.name; // context arrow function không có context
+    }
+}
+
+console.log(course.getName())
+
+
+
+// arrow function trong trường hợp này sẽ bị lỗi
+let Course = (name, addrest) => {
+    this.name = name;
+    this.addrest = addrest
+} 
+
+let jsCourse =  new Course ('Huu Hung', 'Viet Nam')
+console.log(jsCourse)
+
