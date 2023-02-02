b8aa2c6a2d8e8d55d53cecae269b284dfca25c6c
Huu Hung <huuhungnguyen2002@gmail.com>
2023-02-02 22:35:27 +0700

    add file modfile

--- index.js
+++ index.js
@@ -1,58 +1,17 @@
-// spread: Toán tử giải
-// Lấy ra những element và bỏ đi dấu ngoặc
+// Tagged template literals
+
+function highlight ([first, ...strings], ...values) {
+    return values.reduce(
+        (acc, crr) => [...acc, `<span> ${crr} </span>`, strings.shift()], 
+        [first]
+    )
+    .join('')
+} 
 
 
-// Array
-let array1 = ['Javascript', 'Ruby', 'PHP']
+let branch = 'F8';
+let course = 'Javascript';
 
-let array2 = ['ReactJS', 'Dart']
+let html = highlight `Học lập trình ${course} tại ${branch}!`;
+console.log(html)
 
-let array3 = [...array1,...array2]
-console.log(array3)
-
-
-// Object
-
-let  object1 = {
-    name: 'Javascript',
-}
-
-let object2 = {
-    price: 1000
-}
-
-let object3 = {
-    ...object1,
-    ...object2,
-}
-
-console.log(object3)
-
-let defaultConfig = {
-    api: 'http://domain-trang-khoa-hoc',
-    apiVersion: 'v1',
-    other: 'other',
-    studyTime: '2 nam'
-}
-
-let exerciseConfig = {
-    ...defaultConfig,
-    api: 'http://domain-trang-chua-bai-tap',
-    version: 400,
-}
-
-
-console.log(exerciseConfig)
-
-
-// Truyen tham so cho ham
-
-let array = ['Javascript', 'PHP', 'Ruby', 'ReactJS']
-
-function logger(...rest) {
-    for(let i = 0; i < rest.length; ++i) {
-        console.log(rest[i])
-    }
-}
-
-logger(...array)
