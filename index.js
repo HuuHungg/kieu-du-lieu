109f55a331bf2960b2cf3b7309ad91c8ade64267
Huu Hung <huuhungnguyen2002@gmail.com>
2023-02-02 21:38:30 +0700

    add file modfile

--- index.js
+++ index.js
@@ -1,23 +1,58 @@
-// rest object
-function logger({name, price, ...rest}) {
-    console.log(name)
-    console.log(price)
-    console.log(rest)
+// spread: Toán tử giải
+// Lấy ra những element và bỏ đi dấu ngoặc
 
-}
 
-logger({
+// Array
+let array1 = ['Javascript', 'Ruby', 'PHP']
+
+let array2 = ['ReactJS', 'Dart']
+
+let array3 = [...array1,...array2]
+console.log(array3)
+
+
+// Object
+
+let  object1 = {
     name: 'Javascript',
-    price: 1000,
-    description: 'Description content'
-})
-
-// array
-
-function logger2([a,b, ...rest2]) {
-    console.log(a)
-    console.log(b)
-    console.log(rest2)
 }
 
-logger2([2,6,1,2,3,4,5,5,6])
+let object2 = {
+    price: 1000
+}
+
+let object3 = {
+    ...object1,
+    ...object2,
+}
+
+console.log(object3)
+
+let defaultConfig = {
+    api: 'http://domain-trang-khoa-hoc',
+    apiVersion: 'v1',
+    other: 'other',
+    studyTime: '2 nam'
+}
+
+let exerciseConfig = {
+    ...defaultConfig,
+    api: 'http://domain-trang-chua-bai-tap',
+    version: 400,
+}
+
+
+console.log(exerciseConfig)
+
+
+// Truyen tham so cho ham
+
+let array = ['Javascript', 'PHP', 'Ruby', 'ReactJS']
+
+function logger(...rest) {
+    for(let i = 0; i < rest.length; ++i) {
+        console.log(rest[i])
+    }
+}
+
+logger(...array)
