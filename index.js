981f0fbb4c04097ad14fabf7b6d955a00dc129a2
Huu Hung <huuhungnguyen2002@gmail.com>
2023-02-03 20:33:35 +0700

    add file modfile

--- index.js
+++ index.js
@@ -1,35 +1,54 @@
 
-// Optional Chaining
+// IIFE
+// - Dùng dấu ; trước IIFE
+// - IIFE Là hàm "private" có tính riêng tư muốn truy cập vào nó thì không truy cập được
+// - Những đoạn code viết trong iife sẽ không bị ảnh hưởng đến biến toàn cục
+// - Bên ngoài không thể truy cập được vào bên trong vì thế người ta dùng iife
 
-let obj = {
-    name: 'Alice',
-    cat: {
-        name: 'Dinah',
-        cat2: {
-            name: 'Dinah2',
-                // cat3:{
-                //     name: 'Dinah3'
-                // }
+
+
+
+;(function myFunc(message) {
+    console.log('Message', message)
+}) ('Huu Hung Nguyen')
+
+// myFunc() Khi truy cập ở đây thì nó sẽ không truy cập được 
+
+let i = 0
+
+;(function myFunc2() {
+    i++
+    console.log(i)
+    if(i < 5) 
+        myFunc2()
+}) ()
+
+
+;(function() {
+    let fullName = 'Huu Hung Nguyen'
+    console.log(fullName)   
+})()
+
+
+
+const app = (function() {
+    // Private
+    
+    const cars = []
+
+    return {
+        get(index) {
+            return cars[index]
+        },
+        add(car) {
+            cars.push(car)
+        },
+        edit(index, car) {
+            cars[index] = car
+        },
+        delete(index) {
+            cars.splice(index, 1)
         }
     }
-}
-// Khi chúng ta không tin cái key đó có tồn tại hay không thì chúng ta thêm cái option chaining vào
+})()
 
-if(obj.cat?.cat2?.cat3) {
-    console.log(obj.cat.cat2.cat3.name)
-}
-
-// Cách sử dụng Option Chaining
-
-// obj?.prop
-// obj?.[expr]
-// arr?.[index]
-// func?.(args)
-
-let obj2 = {
-    getName(value) {
-        console.log(value)
-    }
-}
-
-obj2.getName?.(123)
