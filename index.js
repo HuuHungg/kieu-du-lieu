de959b74ec25d73469cf5283ef97c7b266e8144b
Huu Hung <huuhungnguyen2002@gmail.com>
2023-02-06 11:14:37 +0700

    add file modfile

--- index.js
+++ index.js
@@ -35,3 +35,64 @@ let teacher2 = {
 
 let button = document.querySelector('button')
 button.onclick = teacher2.getFullName.bind(teacher2)
+
+
+
+let $ = document.querySelector.bind(document)
+let $$ = document.querySelectorAll.bind(document)
+console.log($('#heading'))
+
+
+
+const app = (() => {
+    let cars = ['BMW', 'Mercedes']
+    
+    const root = $('#root')
+    const input = $('#input')
+    const submit = $('#submit')
+
+
+    return {
+        add(car) {
+            cars.push(car)
+        },
+        delete(index) {
+            cars.splice(index,1)
+        },
+        render() {
+            const html = cars.map((car,index) => `
+                <li>
+                    ${car}  
+                    <span class="delete" data-index="${index}"> x </span>
+                 </li>
+            `)
+            .join('')
+            root.innerHTML = html
+        },
+        handleDelete(e) {
+            let deleteBtn = e.target.closest('.delete')
+            if (deleteBtn) {
+                const index = deleteBtn.dataset.index
+                console.log(this)
+                this.delete(index)
+                this.render()
+            }
+        }, 
+        init() {
+            // Handle DOM events
+            submit.onclick = () => {
+                    let car = input.value
+                    this.add(car)
+                    this.render()
+
+                    input.value = null
+                    input.focus()
+            }
+
+            root.onclick = this.handleDelete.bind(this)
+            this.render()
+        }   
+    }
+})()
+app.init()
+// Delegate()
--- scope.md
+++ scope.md
@@ -1,12 +1,2 @@
-Từ khoá this trong Javascript đề cập đến đối tượng mà nó thuộc về
-## Đặc tính
-    1. Trong một phương thức, this tham chiếu tới đối tượng truy cập phương thức (đối tượng trước dấu.)
-    2. Đứng ngoài phương thức this tham chiếu tới đối tượng Global
-##  
-    - This trong hàm tạo là đại diện cho đối tượng sẽ được tạo
-    - This trong một hàm là undefine khi ở strice mode
-    
-## 
-    - Các phương thức bind() call() apply() có thểm tham chiếu this tới đối tượng khác
-    - Trong hầu hết các trường hợp thì this trả về đối tượng mà nó thuộc về,
-
+- Phương thức bind() sẽ trả về một hàm mới với cái this mới 
+- 
